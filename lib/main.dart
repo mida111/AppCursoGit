067bb110b297c82e1d85ee668e1283c42506f531
mida111 <65840119+mida111@users.noreply.github.com>
2025-04-04 09:37:56 -0600

    Se cambio el titulo de la aplicacion

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Curso Git',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'GitHub'),
     );
   }
 }
